b580db164a0349cf2333391440650b4c55ab7dc2
borisescudero <85656598+borisescudero@users.noreply.github.com>
2021-09-23 12:02:40 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -72,7 +72,7 @@ function LED1_Off(){
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor").innerHTML=message.payloadString;
+	  document.getElementById("sensor1").innerHTML=message.payloadString;
 
   }
   function LED1_On(){
